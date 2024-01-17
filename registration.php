fe68f41513ab19a0c248d5033be5e7b3976c7859
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2024-01-17 21:41:07 +0300

    registration.php

--- registration.php
+++ registration.php
@@ -2,10 +2,82 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Event Registration</title>
+    <title>Registration</title>
+    <link rel="stylesheet" href="./static/frameworks/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./static/css/site.css"/>
+    <link href="./static/img/favicon.ico" rel="shortcut icon" type="image/x-icon">
 </head>
-<body>
-    
+<body class="bg-dark" stytle="height: 100vh">
+
+    <div class=" d-flex  text-center justify-content-center h-100 align-items-center">
+
+  
+
+     <div class=" bg-light  text-center justify-content-center  p-5  loginBox w-100 m-5">
+       
+        <div class="row justify-content-center">
+            <h3>
+                Registration to attend [Electric  event] Start in [Jan-31-2024] End in [Jan-31-2024]
+            </h3>
+        </div>
+
+        <table class="table m-3">
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <input type="text" class="form-control">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Mobile</td>
+                    <td>
+                        <input type="text" class="form-control">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Email</td>
+                    <td>
+                        <input type="email" class="form-control">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Company</td>
+                    <td>
+                        <input type="email" class="form-control">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Job Title</td>
+                    <td>
+                        <input type="email" class="form-control">
+                    </td>
+                </tr>
+            </thead>
+
+           
+       </table>
+       <hr>
+
+       <div class="row justify-content-center">
+        <button class="btn btn-akhBule">Register</button>
+       </div>
+
+    </div>
+ 
+
+
+
+    </div>
+
+
+<script src="./static/frameworks/jquery/dist/jquery.min.js"></script>
+<script src="./static/frameworks/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
