2d16df053b739290f5a3b3ffe7137eea2fd087fc
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2024-01-20 12:17:05 +0300

    Contact not Registred add new contact

--- addRegister.php
+++ addRegister.php
@@ -17,7 +17,7 @@ $eventId  = $_POST["eventId"];
 require("auth/connection.php");
 $id = $conn->real_escape_string($eventId); // Escaping special characters
 $id = intval($eventId); // Converting to integer
-$stmt = $conn->prepare("SELECT `id`, `subject`, `start_date`,`start_time`, `end_date`,`end_time` FROM event WHERE id = ? LIMIT 1;");
+$stmt = $conn->prepare("SELECT `id`, `subject`, `start_date`,`start_time`, `end_date`,`end_time` FROM `event` WHERE id = ? LIMIT 1;");
 $stmt->bind_param("i", $id); // Bind the sanitized value            
 // Execute the statement
 $stmt->execute();
@@ -29,8 +29,21 @@ if($result->num_rows <= 0)
 }
 else
 {
-    // check if new contact add to our address book
+    // check if the event expired or there is not time left !
+    // check if new contact exist if not add to our address book
+    $sql = $conn->prepare("SELECT `email` FROM `address_book` WHERE email = ? LIMIT 1;");
+    $sql->bind_param("s",$email );
+    $sql->execute();
+    $result2 = $sql->get_result();
+    if($result2->num_rows <= 0)
+    {
+       //Contact not Registred add new contact 
+    }
+    else
+    {
+        //Contact Exist don't add the contact 
+    }
     // add 1 to event for total_registered
-    // add record in table = attendance 
+    // add record in table = attendance but check also if already registred ! 
 }
 ?>
---
+++ setup/database/attendance.php
@@ -0,0 +1,29 @@
+<?php
+$sql = "
+
+CREATE TABLE IF NOT EXISTS `attendance` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `contact_id` INT(11) DEFAULT NULL,
+    `event_id` INT(11) DEFAULT NULL,
+    `created_date` DATETIME(6) DEFAULT NULL,
+    `attended_date` DATE DEFAULT NULL,
+    `attended_time` DATE DEFAULT NULL,
+    `last_modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+";
+
+try
+{
+ $SQLresult = $conn->query($sql);
+ echo("<li> <i class='fas fa-check m-2'></i> address book table has been created </li>"); 
+}
+catch(PDOException $e)
+{
+ echo "<p class='bg-warning'>".$e->getMessage()."</p>";
+     echo "<br/>";
+}
+
+
+?>
