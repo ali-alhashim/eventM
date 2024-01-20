9da1a30c3b666d6c4059a643b5f04eadf72c80c7
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2024-01-20 21:51:15 +0300

    this first time add him to attendance table

--- addRegister.php
+++ addRegister.php
@@ -12,7 +12,7 @@ $jobTitle = $_POST["jobtitle"];
 
 $eventId  = $_POST["eventId"];
 
-
+$currentDate  = date('Y-m-d H:i:s');
 
 require("auth/connection.php");
 $id = $conn->real_escape_string($eventId); // Escaping special characters
@@ -38,12 +38,49 @@ else
     if($result2->num_rows <= 0)
     {
        //Contact not Registred add new contact 
+       $sql = "INSERT INTO `address_book`(`name`, `email`, `created_by`, `created_date`, `mobile`, `company`, `job_title`) VALUES('$name', '$email', 'self', '$currentDate', '$mobile', '$company', '$jobTitle');";
+
+            try
+                {
+                $SQLresult = $conn->query($sql);
+                $sql = "SELECT `email` FROM `address_book` WHERE email = ? LIMIT 1;";
+                $sql->bind_param("s",$email );
+                $sql->execute();
+                $result3 = $sql->get_result();
+                $row = $result3->fetch_array(MYSQLI_ASSOC);
+                $ContactId = $row["id"];
+                }
+            catch(PDOException $e)
+                {
+                echo "<p class='bg-warning'>".$e->getMessage()."</p>";
+                    echo "<br/>";
+                }
     }
     else
     {
         //Contact Exist don't add the contact 
+        $row = $result2->fetch_array(MYSQLI_ASSOC);
+        $ContactId = $row["id"];
     }
     // add 1 to event for total_registered
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    $oldTotalRegistred = $row["total_registered"];
+    $updatedTotalRegistred = $oldTotalRegistred + 1;
+    $sql = "UPDATE `event` SET `total_registered` = $updatedTotalRegistred WHERE id=$id;";
+    $SQLresult = $conn->query($sql);
     // add record in table = attendance but check also if already registred ! 
+
+    $sql = "SELECT `contact_id`, `event_id` FROM `attendance` where `event_id`=$id  AND contact_id=$ContactId LIMIT 1;";
+    $result = $conn->query($sql);
+    if($result->num_rows <= 0)
+    {
+       // this first time add him to attendance table
+    }
+    else
+    {
+       // he already registred !
+    }
+     
+    //send email for confirmation of registration with QR code 
 }
 ?>
